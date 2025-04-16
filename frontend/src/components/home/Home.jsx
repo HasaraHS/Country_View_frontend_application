10e0543404e3c380d3fa05a253541aedaa52720f
IT22342058 <IT22342058@my.sliit.lk>
2025-04-16 15:20:31 +0530

    nav bar

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../navbar/Navbar";
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className={`h-[1080px] overflow-hidden`} id="home">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home;
